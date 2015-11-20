5460f1228610a78e9e00a5cd90020337fd93a5dc
maikeld86 <m.doemges86@ziggo.nl>
2015-11-20 15:58:11 +0100

    Content dmca template at  -6.54

---
+++ app/Http/Templates/dmca.blade.php
@@ -0,0 +1,20 @@
+i am the copyright owner of the content,"{{ $infringing_title }}" being infringed at:
+
+{{ $infringing_title }}
+
+You may find the original content at:
+
+{{ $original_link }}
+
+{{ $original_description }}
+
+This letter is official notification under the provisions of Section 512(c) of the Digital Millenium Copyright Act ("DMCA") to Effect --
+
+i have a good fait belief that the use of the material in the manner complained of is not authorized by the copuright owner, --
+
+please send me at the address noted below a prompt response indicating the actions you have taken to resolve this matter.
+
+Sincerely,
+
+{{ $name }}
+{{ $email }}
